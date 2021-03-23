ac4d23de453761106ac4d87412103a8f26ad76ad
danielcomerio <danielhcomerio@gmail.com>
2021-03-22 22:17:32 -0300

    'a'

--- index.js
+++ index.js
@@ -3,9 +3,6 @@ var app = express();
 var path = require('path');
 
 app.use(express.static(path.join(__dirname)));
-app.use("/styles", express.static(__dirname));
-app.use("/images", express.static(__dirname + '/images'));
-app.use("/scripts", express.static(__dirname + '/scripts'));
 
 // viewed at based directory http://localhost:8080/
 app.get('/', function (req, res) {
