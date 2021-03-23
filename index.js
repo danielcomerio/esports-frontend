0e23e37f6e7ae6fa73b649b03c7075aece846e3e
danielcomerio <danielhcomerio@gmail.com>
2021-03-22 22:45:01 -0300

    'a'

--- index.js
+++ index.js
@@ -10,13 +10,13 @@ const http = require('http')
 const fs = require('fs')
 var pat = 'https://esportes-frontend.herokuapp.com/';
 
-// viewed at based directory http://localhost:8080/
-app.get('/', function (req, res) {
-    res.sendFile(path.join(pat + 'pages/home.html'));
-});
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'content-type': 'text/html' })
+    fs.createReadStream('pages/home.html').pipe(res)
+})
 
 // add other routes below
-app.get('/cadastro', function (req, res) {
+app.get('/cadastro.html', function (req, res) {
     res.redirect(pat + 'pages/cadastro.html');
 });
 app.get('/carrinho', function (req, res) {
